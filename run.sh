75bb4fbb2b349026041e4c744bdb6ea3597b19fa
Auro Tripathy <ipserv@yahoo.com>
2020-11-04 06:32:46 +0000

    init

---
+++ run.sh
@@ -0,0 +1,3 @@
+#HSA_FORCE_FINE_GRAIN_PCIE=1 NCCL_MIN_NCHANNELS=4 python3.6 moded_micro_benchmarking_pytorch.py --network ResNeXt101_32C_48d --dataparallel --device_ids=0,1,2,3,4,5,6,7 --iterations 10 --batch-size 128
+HSA_FORCE_FINE_GRAIN_PCIE=1 NCCL_MIN_NCHANNELS=4 python3.6 moded_micro_benchmarking_pytorch.py --network ResNeXt101_32C_48d --dataparallel --device_ids=0,1 --iterations 10 --batch-size 16
+
